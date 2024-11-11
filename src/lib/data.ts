45b1e0ff8034fd3b264e35a48e2ba6e40da08017
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-11 16:29:04 +1100

    feat: create page skeleton

---
+++ src/lib/data.ts
@@ -0,0 +1,32 @@
+export const links = [
+  { name: 'GitHub', url: 'https://github.com/kawarimidoll' },
+  { name: 'Bluesky', url: 'https://bsky.app/profile/kawarimidoll.bsky.social' },
+  { name: 'Zenn', url: 'https://zenn.dev/kawarimidoll' },
+  { name: 'しずかなインターネット', url: 'https://sizu.me/kawarimidoll' },
+  { name: 'connpass', url: 'https://connpass.com/user/kawarimidoll/' },
+  { name: 'Exercism', url: 'https://exercism.org/profiles/kawarimidoll' },
+  { name: 'YouTube', url: 'https://youtube.com/@kawarimidoll' },
+]
+
+export const talks = [
+  {
+    name: 'Svelte Japan Online Meetup #5 (2024-11-08)',
+    event: 'https://svelte-jp.connpass.com/event/329938/',
+    slides: 'https://docs.google.com/presentation/d/1ECYinHkkS3K3j3Y8-Zqoae9lesSXziH0dzqevOsz9rI/edit?usp=sharing',
+  },
+  {
+    name: 'October 2024 Rust Melbourne Meetup (2024-10-28)',
+    event: 'https://www.meetup.com/rust-melbourne/events/304034898/',
+    slides: 'https://docs.google.com/presentation/d/197KjWTTnq8Qqlfy3DBUPw2Xr4maRhrc0I2rJ8qvrvzs/edit?usp=sharing',
+  },
+  {
+    name: 'Gorilla.vim #29 (2023-12-13)',
+    event: 'https://gorillavim.connpass.com/event/301953/',
+    slides: 'https://docs.google.com/presentation/d/1A_wQYx1MNidw3lxMfRSsqNjAKCqwhe4OF4-VUyk7wgc/edit?usp=sharing',
+  },
+  {
+    name: 'VimConf 2023 tiny LT (2023-11-18)',
+    event: 'https://vimconf.org/2023/',
+    slides: 'https://docs.google.com/presentation/d/164qZzSwB02lMntTHq4Cx__36AIPBfk3fWLR71ZxLdCE/edit?usp=sharing',
+  },
+]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href='https://svelte.dev/docs/kit'>svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { links, talks } from '$lib/data.ts'
+</script>
+
+<h1>kawarimidoll</h1>
+<p>カワリミ人形</p>
+
+<h2 id='links'>Links</h2>
+<dl>
+  {#each links as { name, url }}
+    <dt>{name}</dt>
+    <dd><a href={url} target='_blank'>{url}</a></dd>
+  {/each}
+</dl>
+
+<h2 id='talks'>Talks</h2>
+<dl>
+  {#each talks as { name, event, slides }}
+    <dt>{name}</dt>
+    <dd>
+      <a href={event} target='_blank'>event details</a>
+      <a href={slides} target='_blank'>slides</a>
+    </dd>
+  {/each}
+</dl>
