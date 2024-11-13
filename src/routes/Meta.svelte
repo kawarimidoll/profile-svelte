6d219a1524e1692c332a41d15f6a2b4e30b8de59
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-14 10:16:26 +1100

    feat: use svelte-meta-tags

--- src/routes/Meta.svelte
+++ src/routes/Meta.svelte
@@ -1,31 +1,38 @@
 <script lang='ts'>
+  import { MetaTags } from 'svelte-meta-tags'
+
+  const name = 'kawarimidoll'
   const title = 'kawarimidoll.github.io/profile-svelte'
   const url = 'https://kawarimidoll.github.io/profile-svelte'
   const description = 'after all, kawarimidoll'
   const image = {
-    src: 'https://avatars.githubusercontent.com/u/8146876?s=200',
+    url: 'https://avatars.githubusercontent.com/u/8146876?s=200',
     alt: 'kawarimidoll\'s icon',
     type: 'image/png',
+    width: 200,
   }
 </script>
 
-{#snippet ogMeta(property: string, content: string)}
-  <meta property='og:{property}' content={content} />
-{/snippet}
-
-<svelte:head>
-  <title>{title}</title>
-  <meta name='description' content={description} />
-
-  <!-- Open Graph -->
-  {@render ogMeta('url', url)}
-  {@render ogMeta('type', 'profile')}
-  {@render ogMeta('title', title)}
-  {@render ogMeta('description', description)}
-  {@render ogMeta('image', image.src)}
-  {@render ogMeta('image:alt', image.alt)}
-  {@render ogMeta('image:type', image.type)}
-
-  <meta property='profile:username' content='kawarimidoll' />
-  <link rel='canonical' href={url} />
-</svelte:head>
+<MetaTags
+  {title}
+  {description}
+  canonical={url}
+  openGraph={{
+    url,
+    title,
+    description,
+    type: 'profile',
+    profile: { username: name },
+    images: [image],
+    siteName: title,
+  }}
+  twitter={{
+    creator: `@${name}`,
+    site: `@${name}`,
+    cardType: 'summary_large_image',
+    title,
+    description,
+    image: image.url,
+    imageAlt: image.alt,
+  }}
+/>
