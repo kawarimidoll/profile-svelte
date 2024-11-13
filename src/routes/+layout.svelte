581715b295d02b04421826f902e3598dace8e39e
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-13 17:32:35 +1100

    feat: update meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,13 @@
   import { i18n } from '$lib/i18n'
   import { ParaglideJS } from '@inlang/paraglide-sveltekit'
   import { ModeWatcher } from 'mode-watcher'
+  import Meta from './Meta.svelte'
   import 'uno.css'
 
   const { children } = $props()
 </script>
 
+<Meta />
 <ModeWatcher />
 
 <ParaglideJS {i18n}>
---
+++ src/routes/Meta.svelte
@@ -0,0 +1,31 @@
+<script lang='ts'>
+  const title = 'kawarimidoll.github.io/profile-svelte'
+  const url = 'https://kawarimidoll.github.io/profile-svelte'
+  const description = 'after all, kawarimidoll'
+  const image = {
+    src: 'https://avatars.githubusercontent.com/u/8146876?s=200',
+    alt: 'kawarimidoll\'s icon',
+    type: 'image/png',
+  }
+</script>
+
+{#snippet ogMeta(property: string, content: string)}
+  <meta property='og:{property}' content={content} />
+{/snippet}
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name='description' content={description} />
+
+  <!-- Open Graph -->
+  {@render ogMeta('url', url)}
+  {@render ogMeta('type', 'profile')}
+  {@render ogMeta('title', title)}
+  {@render ogMeta('description', description)}
+  {@render ogMeta('image', image.src)}
+  {@render ogMeta('image:alt', image.alt)}
+  {@render ogMeta('image:type', image.type)}
+
+  <meta property='profile:username' content='kawarimidoll' />
+  <link rel='canonical' href={url} />
+</svelte:head>
