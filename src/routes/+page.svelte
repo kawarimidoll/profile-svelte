613cc9b22704936ba3cc0dbbdfd2e5cd1850d0c2
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-13 12:49:49 +1100

    feat: click main image to toggle dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,29 @@
   import TypeWriter from '$lib/components/TypeWriter.svelte'
   import { links, talks } from '$lib/data'
   import { toggleMode } from 'mode-watcher'
+  import { Body } from 'svelte-body'
 </script>
 
 {#snippet exLink(url: string, title?: string)}
   <a href={url} target='_blank' class='break-all text-blue-600 dark:text-blue-500'>{title || url}</a>
 {/snippet}
 
-<main uno-mx-auto uno-mt-6 uno-mb-6 uno-p-8 uno-border='~ solid' uno-max-w-screen-sm uno-font='normal sans' uno-text-lg>
-  <figure uno-flex='~' uno-justify='center' uno-align='center' uno-m-0 uno-mb-2>
-    <img src='https://avatars.githubusercontent.com/u/8146876' alt='avatar' class='size-64 rounded-full' />
-  </figure>
+<Body class='mx-4 bg-slate-50 text-slate-800 transition-colors dark:bg-slate-900 dark:text-slate-50' />
+
+<main uno-mx-auto uno-mt-6 uno-mb-2 uno-p-8 uno-border='~ solid color-zinc-400 dark:color-slate-400' uno-max-w-screen-sm uno-font='normal sans' uno-text-lg>
+  <div uno-flex='~' uno-justify='center' uno-align='center' uno-m-0 uno-mb-2>
+    <button aria-label='avatar image' onclick={toggleMode} uno-border-none
+            uno-size-64 uno-rounded-full uno-bg='center cover'
+            class="bg-[url('https://avatars.githubusercontent.com/u/8146876')]">
+    </button>
+  </div>
 
   <h1 uno-text='center 4xl' uno-font-medium uno-mb-5>kawarimidoll</h1>
   <p uno-text-center>カワリミ人形</p>
-  <button onclick={toggleMode}>Toggle Mode</button>
 
-  <blockquote uno-italic uno-m-0 uno-py-0 uno-px-4 uno-border='0 l-4 solid'>
+  <blockquote uno-italic uno-m-0 uno-py-0 uno-px-4 uno-border='0 l-4 solid color-zinc-400 dark:color-slate-400'>
     <TypeWriter text='は〜〜安全圏から他人の恋愛のゴタゴタを観測して楽しみてぇな〜〜〜' />
-    <footer uno-m-0 uno-p-0 uno-text='sm right zink-300 dark:zink-400'>
+    <footer uno-m-0 uno-p-0 uno-text='sm right zinc-400 dark:zinc-400'>
       - kawarimidoll,
       {@render exLink('https://bsky.app/profile/kawarimidoll.bsky.social/post/3l5tr342umc2o', '2024-10-06')}
     </footer>
@@ -45,48 +50,8 @@
   </dl>
 </main>
 
-<footer uno-text='sm center' uno-pb-4>
+<footer uno-text='sm center zinc-400 dark:slate-400' uno-pb-4>
   You can find the source code for this page in
   {@render exLink('https://github.com/kawarimidoll/kawarimidoll.github.io', 'this repository')}
   .
 </footer>
-
-<style>
-
-:root {
-  color-scheme: light dark;
-
-  --light-color: #1a1919;
-  --light-gray: #bbbbcb;
-  --light-link: #1218ee;
-  --light-visited: #551a8b;
-  --dark-color: #fbfdfb;
-  --dark-gray: #666696;
-  --dark-link: #4376ef;
-  --dark-visited: #8e40d6;
-}
-
-:global(body) {
-  color: light-dark(var(--light-color), var(--dark-color));
-  background-color: light-dark(var(--dark-color), var(--light-color));
-  transition:
-    background-color 0.5s ease,
-    color 0.5s ease;
-}
-
-main, blockquote {
-  border-color: light-dark(var(--light-gray), var(--dark-gray));
-}
-
-/* a { */
-/*   color: light-dark(var(--light-link), var(--dark-link)); */
-/*   &:visited { */
-/*     color: light-dark(var(--light-visited), var(--dark-visited)); */
-/*   } */
-/* } */
-
-footer {
- /* zinc-400 dark:slate-400 */
-  color: light-dark(var(--light-gray), var(--dark-gray));
-}
-</style>
