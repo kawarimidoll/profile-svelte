2134e6e285bb1bd5005fd636bbe4eca00cf64148
kawarimidoll <kawarimidoll+git@gmail.com>
2024-11-17 12:59:22 +1100

    feat: use svelte-body

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
   import { i18n } from '$lib/i18n'
   import { ParaglideJS } from '@inlang/paraglide-sveltekit'
   import { ModeWatcher } from 'mode-watcher'
+  import { Body } from 'svelte-body'
   import Meta from './Meta.svelte'
   import 'uno.css'
 
@@ -14,3 +15,5 @@
 <ParaglideJS {i18n}>
   {@render children()}
 </ParaglideJS>
+
+<Body class='mx-4 bg-slate-50 text-slate-800 font-sans transition-colors dark:bg-slate-900 dark:text-slate-50' />
